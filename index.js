771ba10a08330089c6dcaf13556b2c4446a04ecd
Tushar <tushargahlaut74@gmail.com>
2022-06-21 12:04:28 +0530

    Fix Bug

--- index.js
+++ index.js
@@ -12,19 +12,12 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-//setup routes
 
-app.use("/users", userRouter);
-app.use("/admin",isAdmin,problemRouter);
 
-//Fix Bug
-app.get("/",(req,res)=>{
-  res.send("Welcome to my API");
-})
+// connect to MongoDB
 
 const port = process.env.PORT || 5000;
 
-// connect to MongoDB
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -32,3 +25,16 @@ mongoose
     app.listen(port, () => console.log(`Server running on port ${port}`));
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+  app.get("/",(req,res)=>{
+    res.send("Welcome to my API");
+  })
+  
+
+//setup routes
+app.use("/users", userRouter);
+app.use("/admin",isAdmin,problemRouter);
+
+//Fix Bug
+
+
